2643d7b4e42ae5492a3e73401c4936322ea30c99
Hossein bonyadi <hosseinbonyadi2@gmail.com>
2024-12-25 09:04:04 +0330

    Add handlers : Add task , Delete task one by one ,Delete all tasks

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,30 +3,34 @@ import { useState } from "react";
 export default function Todo(){
     const [task, setTask] = useState('');
     const [todo, setTodo] = useState([]);
-    const [todoUpdate, setTodoUpdate] = useState([]);
 
     const Task = (e) => {
         setTask(e.target.value);
     }
     const addHandler = () => {
-        setTodo([...todo,task]);
+        setTodo([...todo, task]);
     }
-    const deletTask = (index) => {
-        console.log(index);
-        setTodo([...todo.filter((item,i)=>i!==index)]);
+    const deleteTask = (index) => {
+        setTodo(todo.filter((item, i) => i !== index));
     }
+    const DeleteAll = () => {
+        setTodo([]);
+        
+    }
+    
 
     return (
       <div>
         <h1>Todo</h1>
         <input type="text" name="inputTask" onChange={(e)=>{Task(e)}} placeholder="Add a new todo"/>
         <button onClick={addHandler}>Add</button>
+        <button onClick={DeleteAll}>Delete Tasks</button>
 
 
 
         <ul>
-            {todo.map((item,index)=>{
-                return <li key={index}>{item} <button onClick={()=>{deletTask(index)}}>x</button></li>
+            {todo.map((item, index) => {
+                return <li key={index}>{item} <button onClick={() => { deleteTask(index) }}>x</button></li>
             })}
         </ul>
 
